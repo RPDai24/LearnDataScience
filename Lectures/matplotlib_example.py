b618560e5668c2ffe03b06362fffbef3fd8c1375
RPD <hupochuan80@icloud.com>
2024-04-23 12:26:45 +0100

    add directory

---
+++ Lectures/matplotlib_example.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+import seaborn as sns
+
+# print(matplotlib.__version__)
+# Histograms, subplots
+# N = 100
+# x = np.random.rand(N)
+# # print(x)
+# plt.hist(x, color="b")
+# plt.show()
+
+
+print(sns.get_dataset_names())
---
+++ Lectures/panda_example.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+url = "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv"
+df = pd.read_csv(url)
+
+# print(df.head)
+# print(df.shape)
+# print(df.info())
+# print(df.describe)
+
+print(df.columns)
+
+print('Hello', 'how are you?')
